1c16bb1a31382f828c2cc5d50fe1382ffb39ef18
Tan, Shiqiang <1214091885@qq.com>
2019-06-02 01:01:21 +0800

    init

---
+++ src/main/java/com/sq/main/CampusServerApplication.java
@@ -0,0 +1,13 @@
+package com.sq.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sq/main/CampusServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sq.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CampusServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
